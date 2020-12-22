wikimedia/operations-software-klaxon

commit ba13da2e144782397159a8801500e5dd1aef3672

Initial commit of Klaxon, a webapp for trusted users to page SRE
A simple, mobile-friendly, user-friendly webapp.

Allows trusted users to page us via SRE's usual oncall paging mechanism
(protected by our developer account SSO).

Funnels users to Phab or IRC for non-urgent issues.

Bug: T270324
Change-Id: I697b327a4557f14dc9c5c312cb89842bbeb634ae

klaxon/__init__.py
@@ -1,0 +1,122 @@
+"""A webapp to allow trusted users to manually page SRE."""
+
+__author__ = "Chris Danis"
+__version__ = "0.1.0"
+__license__ = "GNU AGPL v3.0"
+__repository__ = "https://gerrit.wikimedia.org/r/plugins/gitiles/operations/software/klaxon"
+__copyright__ = """
+Copyright © 2020 Chris Danis & the Wikimedia Foundation
+
+This program is free software: you can redistribute it and/or modify it under the terms
+of the GNU Affero General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with
+this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import datetime
+import operator
+import os
+import threading
+
+import cachetools
+import werkzeug.exceptions
+from flask import Flask, flash, redirect, render_template, request
+
+from klaxon.victorops import VictorOps
+
+CONFIG_DEFAULTS = {
+    'KLAXON_REPOSITORY': __repository__,
+    'KLAXON_INCIDENT_LIST_CACHE_TTL_SECONDS': 10,
+    'KLAXON_INCIDENT_LIST_RECENCY_MINUTES': 60,
+    'KLAXON_CAS_AUTH_HEADER': 'CAS-User',
+    'KLAXON_VO_API_ID': None,
+    'KLAXON_VO_API_KEY': None,
+    'KLAXON_VO_CREATE_INCIDENT_URL': None,
+    'KLAXON_SECRET_KEY': None,
+    'KLAXON_ADMIN_CONTACT_EMAIL': None,
+}
+
+
+def create_app():
+    app = Flask(__name__)
+    if app.config['ENV'] == 'development':
+        app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+    for key, default in CONFIG_DEFAULTS.items():
+        app.config[key] = os.environ.get(key, default=default)
+    # Needed for Flask flash() support.
+    app.secret_key = app.config['KLAXON_SECRET_KEY']
+
+    # VictorOps aka Splunk On-Call has rate limits on their API.
+    # So, a Klaxon instance does some local caching of API calls, reusing the response for the
+    # list of current incidents for a brief interval.
+    #
+    # This technique only works with the 'gthread' Gunicorn executor model, or similar --
+    # your workers need to share an address space.  (Most of the gevent executors should
+    # also work.)
+    #
+    # for example: gunicorn --worker-class gthread --workers 1 --threads 8 'klaxon:create_app()'
+
+    # Max. 1 item in the cache; TTL duration as configured.
+    api_cache = cachetools.TTLCache(1, app.config['KLAXON_INCIDENT_LIST_CACHE_TTL_SECONDS'])
+    api_lock = threading.RLock()
+
+    vo = VictorOps(api_id=app.config['KLAXON_VO_API_ID'], api_key=app.config['KLAXON_VO_API_KEY'],
+                   create_incident_url=app.config['KLAXON_VO_CREATE_INCIDENT_URL'],
+                   repository=app.config['KLAXON_REPOSITORY'],
+                   admin_email=app.config['KLAXON_ADMIN_CONTACT_EMAIL'])
+
+    @cachetools.cached(api_cache, lock=api_lock)
+    def fetch_victorops():
+        """Return the most recent incidents in reverse chronological order.  Memoized."""
+        max_delta = datetime.timedelta(minutes=app.config['KLAXON_INCIDENT_LIST_RECENCY_MINUTES'])
+        now = datetime.datetime.now(datetime.timezone.utc)
+        rv = [i for i in vo.fetch_incidents() if now - i.time < max_delta]
+        rv.sort(key=operator.attrgetter('time'))
+        rv.reverse()
+        return rv
+
+    def get_username():
+        """From request context, returns the logged-in user or 'unknown' (for local testing)"""
+        header = app.config['KLAXON_CAS_AUTH_HEADER']
+        if app.config['ENV'] == 'production' and header not in request.headers:
+            raise werkzeug.exceptions.Forbidden()
+        return request.headers.get(header, default='unknown')
+
+    @app.route('/')
+    def root():
+        return render_template('index.html')
+
+    @app.route('/recent_incidents')
+    def recent_incidents():
+        """Returns an HTML fragment called by JS to fill in the body of a div of recent alerts."""
+        incidents = fetch_victorops()
+        return render_template('incident_list.html',
+                               incidents=incidents)
+
+    @app.route('/protected/page_form')
+    def page_form():
+        return render_template('page_form.html', username=get_username())
+
+    @app.route('/protected/submit_page', methods=['POST'])
+    def submit_page():
+        form = request.form
+        summary = form['summary']
+        vo.send_page(summary=f"Manual page by {get_username()}: {summary}",
+                     description=form['description'])
+
+        with api_lock:
+            api_cache.clear()
+        # We try to prevent ourselves from caching stale data, but the VictorOps API is only
+        # eventual consistent, so we present this message to the user anyway.
+        flash('Your page was sent. It may not immediately appear in recent alerts, '
+              'but it was sent.')
+        return redirect('/')
+
+    return app

klaxon/templates/incident_list.html
@@ -1,0 +1,29 @@
+{% for incident in incidents %}
+<article class="media{% if loop.index0 == 0 %} mt-2{% endif %}">
+    <figure class="media-left">
+        <p>
+            {% if incident.acked %}
+            <span class="icon is-large" title="Acknowledged page">✅</span>
+            {% else %}
+            <span class="icon is-large" title="Not yet acknowledged">⚠️</span>
+            {% endif %}
+        </p>
+    </figure>
+    <div class="media-content">
+        <div class="content">
+            <div class="has-text-weight-semibold">{{ incident.summary }}</div>
+            {% if incident.acked %}
+            Acknowledged page
+            {% else %}
+            Unacknowledged page
+            {% endif %}
+            &nbsp;<br class="is-hidden-tablet">
+            <small><relative-time datetime="{{ incident.time }}Z">{{ incident.time }} UTC</relative-time></small>
+        </div>
+    </div>
+</article>
+{% else %}
+<div class="has-text-centered mt-4 mb-2">
+    No recent alerts!  Smooth sailing ⛵
+</div>
+{% endfor %}

klaxon/victorops.py
@@ -1,0 +1,123 @@
+"""A friendly shim over the VictorOps (aka Splunk On-Call) API."""
+
+import dateutil.parser
+import datetime
+import logging
+from dataclasses import dataclass
+from typing import AbstractSet, Iterable, Set, Union
+from urllib.parse import urljoin
+
+import requests
+
+import klaxon
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Incident:
+    summary: str
+    acked: bool
+    time: datetime.datetime
+    teams: Set[str]
+
+
+class VictorOpsError(Exception):
+    pass
+
+
+class VictorOps:
+    def __init__(self, *, api_id: str, api_key: str,
+                 create_incident_url: str,
+                 team_ids: Union[None, str, AbstractSet[str]] = None,
+                 admin_email: str,
+                 api_base_url: str = 'https://api.victorops.com/api-public/v1/',
+                 repository: str = klaxon.__repository__):
+        """Creates a VictorOps API wrapper.
+
+        Parameters
+        ----------
+            api_id, api_key : str
+                The API instance identifier and secret key to be used when fetching incidents.
+            create_incident_url : str
+                The URL of the REST integration endpoint to be used to create new incidents.
+                See https://help.victorops.com/knowledge-base/rest-endpoint-integration-guide/
+            team_ids : AbstractSet[str]
+                A set of team identifiers to filter open incidents.  If empty, no filter.
+            admin_email : str
+                An email address for the administrator of this Klaxon interface, used in our
+                outgoing User-Agent.
+            api_base_url : str
+                The root URL of the VictorOps API.  Used for fetching incidents, but not for
+                creating new ones -- per the instructions at
+                https://portal.victorops.com/public/api-docs.html#!/Incidents/post_api_public_v1_incidents
+        """
+
+        self._create_incident_url = create_incident_url
+        self._api_base_url = api_base_url
+
+        self.team_ids = None
+        if isinstance(team_ids, str):
+            self.team_ids = set([team_ids])
+        elif team_ids:
+            self.team_ids = set(team_ids)
+
+        self._session = requests.Session()
+        # A sample of our outgoing User-Agent:
+        # klaxon/0.1.0 (https://gerrit.wikimedia.org/r/plugins/gitiles/operations/software/klaxon)
+        # instance administered by root@wikimedia.org; python-requests/2.25.0
+        self._session.headers['User-Agent'] = (
+            f"klaxon/{klaxon.__version__} ({repository}) "
+            f"instance administered by {admin_email}; {self._session.headers['User-Agent']}")
+        # It isn't a problem to also include these headers when POSTing to the create_incident_url,
+        # even though it doesn't actually need or use them.
+        self._session.headers['X-VO-Api-Id'] = api_id
+        self._session.headers['X-VO-Api-Key'] = api_key
+
+    def send_page(self, *, summary: str, description: str) -> None:
+        """Creates a new paging incident in VictorOps.
+
+        Parameters
+        ----------
+            summary : str
+                A one-line terse title.  Appears in push notifications.
+            description : str
+                Longer, free-form text.
+
+        Raises:
+        -------
+        requests.HTTPError
+            if the HTTP request failed
+        VictorOpsError
+            if the HTTP request succeded, but the VictorOps API failed
+        """
+        payload = {
+            'message_type': 'CRITICAL',
+            'entity_display_name': summary,
+            'state_message': description,
+        }
+        logging.info("Sending a page: %s", payload)
+        resp = self._session.post(self._create_incident_url, json=payload)
+        resp.raise_for_status()
+        j = resp.json()
+        if j['result'] != 'success':
+            raise VictorOpsError(j.get('message', ''))
+
+    def fetch_incidents(self) -> Iterable[Incident]:
+        """Fetches and yields the current incidents.
+
+        Raises:
+        -------
+        requests.HTTPError
+            if the HTTP request failed
+        """
+        resp = self._session.get(urljoin(self._api_base_url, 'incidents'))
+        resp.raise_for_status()
+        j = resp.json()
+        for i in j['incidents']:
+            if self.team_ids and not set(i['pagedTeams']) & self.team_ids:
+                continue
+            yield Incident(summary=i['service'],
+                           acked=i['currentPhase'] != 'UNACKED',
+                           time=dateutil.parser.isoparse(i['startTime']),
+                           teams=set(i['pagedTeams']))

.gitignore
@@ -1,0 +1,11 @@
+/.coverage
+/.eggs
+/.tox
+/*.egg-info
+__pycache__/
+*.pyc
+*.swp
+/build
+/dist
+.venv
+.vscode

.gitreview
@@ -1,0 +1,5 @@
+[gerrit]
+host=gerrit.wikimedia.org
+port=29418
+project=operations/software/klaxon.git
+defaultbranch=master

klaxon/static/COPYING
@@ -1,0 +1,7 @@
+bulma.min.v0.9.1.css is Bulma v0.9.1 (https://bulma.io),
+licensed under the MIT License (https://opensource.org/licenses/MIT).
+Fetched from https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css
+
+github.time-elements.v3.1.1.js is Github's time-elements (https://github.com/github/time-elements),
+licensed under the MIT License (https://opensource.org/licenses/MIT).
+Fetched from https://unpkg.com/@github/time-elements@3.1.1?module

commit 1b7f5285c2d6106bdb4563e06a1ed76a7e912361

klaxon: address comments from ba13da2
Accidentally submitted before uploading new patch set.

Change-Id: Idbd1727ab6974ded0ead18fa3608a44ff4194586

klaxon/__init__.py
@@ -86,7 +86,7 @@ def create_app():
         """From request context, returns the logged-in user or 'unknown' (for local testing)"""
         header = app.config['KLAXON_CAS_AUTH_HEADER']
         if app.config['ENV'] == 'production' and header not in request.headers:
-            raise werkzeug.exceptions.Forbidden()
+            raise werkzeug.exceptions.Forbidden
         return request.headers.get(header, default='unknown')
 
     @app.route('/')
@@ -107,6 +107,7 @@ def create_app():
     @app.route('/protected/submit_page', methods=['POST'])
     def submit_page():
         form = request.form
+        # TODO: validate that required fields in the form were included.
         summary = form['summary']
         vo.send_page(summary=f"Manual page by {get_username()}: {summary}",
                      description=form['description'])

klaxon/templates/incident_list.html
@@ -18,6 +18,8 @@
             Unacknowledged page
             {% endif %}
             &nbsp;<br class="is-hidden-tablet">
+            {# The relative-time tag is provided by Github's time-elements library:
+               https://github.com/github/time-elements#relative-time #}
             <small><relative-time datetime="{{ incident.time }}Z">{{ incident.time }} UTC</relative-time></small>
         </div>
     </div>

klaxon/victorops.py
@@ -27,6 +27,7 @@ class VictorOpsError(Exception):
 
 
 class VictorOps:
+    # TODO: perhaps these shouldn't be kw-only args
     def __init__(self, *, api_id: str, api_key: str,
                  create_incident_url: str,
                  team_ids: Union[None, str, AbstractSet[str]] = None,
@@ -74,7 +75,7 @@ class VictorOps:
         self._session.headers['X-VO-Api-Id'] = api_id
         self._session.headers['X-VO-Api-Key'] = api_key
 
-    def send_page(self, *, summary: str, description: str) -> None:
+    def send_page(self, summary: str, description: str) -> None:
         """Creates a new paging incident in VictorOps.
 
         Parameters